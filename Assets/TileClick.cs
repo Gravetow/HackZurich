d59226734ea7cb88dd34d67e7ecd7a6d16a8e00b
Gravetow <wruszczak@web.de>
2018-09-15 19:18:28 +0200

    Added stuff

--- Assets/TileClick.cs
+++ Assets/TileClick.cs
@@ -7,6 +7,31 @@ public class TileClick : MonoBehaviour, IPointerClickHandler
     [Inject]
     readonly SignalBus _signalBus;
 
+    private BoxCollider boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        _signalBus.Subscribe<LeaveConstructionSignal>(OnLeaveConstruction);
+        _signalBus.Subscribe<TileClickedSignal>(OnTileClicked);
+    }
+
+    private void OnDestroy()
+    {
+        _signalBus.Unsubscribe<LeaveConstructionSignal>(OnLeaveConstruction);
+        _signalBus.Unsubscribe<TileClickedSignal>(OnTileClicked);
+    }
+
+    public void OnLeaveConstruction(LeaveConstructionSignal leaveConstructionSignal)
+    {
+        boxCollider.enabled = true;
+    }
+
+    public void OnTileClicked(TileClickedSignal tileClickedSignal)
+    {
+        boxCollider.enabled = false;
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         _signalBus.Fire(new TileClickedSignal() { position = transform.position });
