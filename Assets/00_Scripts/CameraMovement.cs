a6a689193296fe824f38c90225c73d41bee45da3
Gravetow <wruszczak@web.de>
2018-09-15 16:16:39 +0200

    Added Camera controls

---
+++ Assets/00_Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class CameraMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+
+    public Transform LookAtTransform;
+
+    public Transform CameraTransform;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        CameraTransform.RotateAround(LookAtTransform.position, -Vector3.up, eventData.delta.x * 5 *  Time.deltaTime);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
+}
---
+++ Assets/GradientBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GradientBackground : MonoBehaviour
+{
+    public Color startColor = Color.red;
+    public Color endColor = Color.blue;
+
+
+    public void Awake()
+    {
+        var mesh = GetComponent<MeshFilter>().sharedMesh;
+        var colors = new Color[mesh.vertices.Length];
+        colors[0] = startColor;
+        colors[1] = endColor;
+        colors[2] = startColor;
+        colors[3] = endColor;
+        mesh.colors = colors;
+    }
+
+}
