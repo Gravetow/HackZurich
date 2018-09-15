41c8a20d4e39e126ec03eecb614ad0b63fde75ed
Gravetow <wruszczak@web.de>
2018-09-15 20:42:34 +0200

    Added carmodel

--- Assets/00_Scripts/GameInstaller.cs
+++ Assets/00_Scripts/GameInstaller.cs
@@ -4,6 +4,7 @@ using Zenject;
 public class GameInstaller : MonoInstaller<GameInstaller>
 {
     public HouseModel houseModel;
+    public CarModel carModel;
 
     public override void InstallBindings()
     {
@@ -14,10 +15,12 @@ public class GameInstaller : MonoInstaller<GameInstaller>
         Container.DeclareSignal<LeaveConstructionSignal>();
 
         Container.Bind<HouseModel>().FromInstance(houseModel);
+        Container.Bind<CarModel>().FromInstance(carModel);
 
         Container.BindInterfacesTo<ResourceView>().AsSingle();
         Container.BindInterfacesTo<TileClick>().AsSingle();
         Container.BindInterfacesTo<CameraMovement>().AsSingle();
         Container.BindInterfacesTo<OverlayView>().AsSingle();
+        Container.BindInterfacesTo<SpawnCars>().AsSingle();
     }
 }
---
+++ Assets/SpawnCars.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class SpawnCars : MonoBehaviour
+{
+
+    [Inject]
+    public CarModel CarModel;
+
+   
+
+
+}
