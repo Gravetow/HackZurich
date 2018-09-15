a019da874da8c4aeb7af1cca649d7c3bff778933
Gravetow <wruszczak@web.de>
2018-09-15 11:38:20 +0200

    Added views

---
+++ Assets/00_Scripts/ResourceSignals.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Zenject;
+
+public class ResourceView : MonoBehaviour
+{
+
+    [Inject] 
+    public TextMeshPro TextMeshPro;
+    
+}
