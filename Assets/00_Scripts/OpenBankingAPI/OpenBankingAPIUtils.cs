bc61061f8ade02223bb3c5e4d80406b40414ddc9
Maximilian Berger <git-commit@users.noreply.github.com>
2018-09-15 20:38:53 +0200

    Add date filter and sum per date to open banking api utils

---
+++ Assets/00_Scripts/OpenBankingAPI/OpenBankingAPIUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class OpenBankingAPIUtils
+{
+    public static IEnumerable<Transaction> keepWithDate(Transaction[] txs, DateTime date)
+    {
+        var formattedDate = String.Format("{0:yyyy-MM-dd}", date.Date);
+        return txs.Where(tx => tx.date.Equals(formattedDate));
+    }
+
+    public static double spendingOnDate(Transaction[] txs, DateTime date)
+    {
+        return keepWithDate(txs, date).Select(tx => tx.amount).Sum();
+    }
+}
